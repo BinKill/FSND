4a94f5adc76e8cfece3df40f92a34206468cfc71
BinKill <BinKill@users.noreply.github.com>
2016-03-21 11:30:55 -0400

    update tv.py
    Updated with suggested changes

--- tv.py
+++ tv.py
@@ -8,36 +8,36 @@ toy_story = media.Movie("Toy Story", # Creates a new Movie instance with the fol
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
 avatar = media.Movie("Avatar",                     
-                     "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+                     "A marine on an alien planet",
                      "December 18, 2009",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 
 the_social_network = media.Movie("The Social Network",
-                                 "The story of Mark Zuckerburg and his creation of popular online Social Network Facebook.",
+                                 "The story of Mark Zuckerburg.",
                                  "October 1, 2010",
                                  "https://upload.wikimedia.org/wikipedia/en/7/7a/Social_network_film_poster.jpg",
                                  "https://www.youtube.com/watch?v=lB95KLmpLR4")
 
 i_am_legend = media.Movie("I Am Legend",
-                          "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.",
+                          "A sole survivor in post apocolyptic New York.",
                           "December 14, 2007",
                           "https://upload.wikimedia.org/wikipedia/en/d/df/I_am_legend_teaser.jpg",
                           "https://www.youtube.com/watch?v=ewpYq9rgg3w")
 
 life_of_pi = media.Movie("Life of Pi",
-                         "A young man who survives a disaster at sea is hurtled into an epic journey of adventure and discovery. While cast away, he forms an unexpected connection with another survivor: a fearsome Bengal tiger.",
+                         "A boy trapped on a boat with a tiger.",
                          "November 21, 2012",
                          "https://upload.wikimedia.org/wikipedia/en/5/57/Life_of_Pi_2012_Poster.jpg",
                          "https://www.youtube.com/watch?v=mZEZ35Fhvuc")
 
 catch_me_if_you_can = media.Movie("Catch me if you Can",
-                                  "A true story about Frank Abagnale Jr., who, before his 19th birthday, successfully conned millions of dollars' worth of checks as a Pan Am pilot, doctor, and legal prosecutor.",
+                                  "A true story of a con artist.",
                                   "December 25, 2002",
                                   "https://upload.wikimedia.org/wikipedia/en/4/4d/Catch_Me_If_You_Can_2002_movie.jpg",
                                   "https://www.youtube.com/watch?v=xas1UyTiVUw")
 
 
-movies = [toy_story, avatar, the_social_network, i_am_legend, life_of_pi, catch_me_if_you_can]
+movies = [toy_story, avatar, the_social_network, i_am_legend, life_of_pi, catch_me_if_you_can] # Adds all movie's too the movie array
 
-fresh_tomatoes.open_movies_page(movies)	
+fresh_tomatoes.open_movies_page(movies)	# Passes the movies array too the open_movies_page() function
